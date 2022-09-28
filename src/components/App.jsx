399a0b881ac540fefd47f3c1bc514111cb19867d
Alejae1998 <alejagr1998@gmail.com>
2022-09-28 11:39:48 -0700

    trying to get something working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-// import { BrowserRouter as Router } from 'react-router-dom';
-//import { Toaster } from 'react-hot-toast';
-// import Routes from './Routes.jsx';
-//import UserProvider from '../state/context/UserContext.jsx';
-//import FuzzyBunnyProvider from '../state/context/FuzzyBunnyContext.jsx';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
 
 export default function App() {
-  // <Router>
-  //   {/* <Toaster />
-  //   <UserProvider>
-  //     <FuzzyBunnyProvider> */}
-  //       <Routes />
-  //     </FuzzyBunnyProvider>
-  //   </UserProvider>
-  // </Router>
+  return (
+    <Router>
+
+    </Router>
+  );
 }
