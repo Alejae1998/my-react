b9632509409873257b07fecc3d809843a3b8f933
Alejae1998 <alejagr1998@gmail.com>
2022-09-30 15:18:06 -0700

    and more

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Routes from './Routes.jsx';
 import UserProvider from '../state/context/UserContext.jsx';
-// import FuzzyBunnyProvider from '../state/context/FuzzyBunnyContext.jsx';
+import FuzzyBunnyProvider from '../state/context/FuzzyBunnyContext.jsx';
 
 export default function App() {
   return (
     <Router>
       <Toaster />
       <UserProvider>
-        {/* <FuzzyBunnyProvider> */}
-        <Routes />
-        {/* </FuzzyBunnyProvider> */}
+        <FuzzyBunnyProvider>
+          <Routes />
+        </FuzzyBunnyProvider>
       </UserProvider>
     </Router>
   );
